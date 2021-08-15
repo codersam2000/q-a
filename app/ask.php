be1876313c6542145a33fc332c92d74015ea35a3
M. Sam Aun <symonh813@gmail.com>
2021-08-15 14:28:19 +0600

    Update ask.php

--- app/ask.php
+++ app/ask.php
@@ -1,11 +1,12 @@
 <?php
 if (isset($_POST['ask'])) {
 	$ques 		= $_POST['ques'];
-	$slug 		= $_POST['ques'];
+	$slug 		= strtolower(str_replace(' ', '-', $ques));
 	$sub_id 	= $_POST['sub-id'];
-	$user_id 	= $_POST['ques'];
-	$points 		= $_POST['points'];
-	$ques_sql 	= "INSERT INTO `question`(`question`, `slug`, `user_id`, `sub_id`, `points`) VALUES ('$ques','$slug','$user_id','$sub_id','$points')";
+	$user_id 	= $_SESSION['user_id'];
+	$points 	= $_POST['points'];
 
+	$ques_sql 	= "INSERT INTO `question`(`question`, `slug`, `user_id`, `sub_id`, `points`) VALUES ('$ques','$slug','$user_id','$sub_id','$points')";
+	
 }
 ?>

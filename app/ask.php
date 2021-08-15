e99f5475f04c888eb8cba14cd55d2100bd874ae7
M. Sam Aun <symonh813@gmail.com>
2021-08-15 13:53:03 +0600

    new version uploaded
    new update

--- app/ask.php
+++ app/ask.php
@@ -1,5 +1,11 @@
 <?php
 if (isset($_POST['ask'])) {
-	echo $_POST['sub-id'];
+	$ques 		= $_POST['ques'];
+	$slug 		= $_POST['ques'];
+	$sub_id 	= $_POST['sub-id'];
+	$user_id 	= $_POST['ques'];
+	$points 		= $_POST['points'];
+	$ques_sql 	= "INSERT INTO `question`(`question`, `slug`, `user_id`, `sub_id`, `points`) VALUES ('$ques','$slug','$user_id','$sub_id','$points')";
+
 }
 ?>
